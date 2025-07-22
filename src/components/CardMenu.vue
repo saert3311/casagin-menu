d03aa60bee36fc42c6440be3d3fc746f9a1de1b6
Jose Duarte <saert@outlook.com>
2025-07-22 19:32:53 +0200

    Logo and basic menu show

---
+++ src/components/CardMenu.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+</script>
+<template>
+  <div class="flex flex-col gap-6">
+    <Card>
+      <CardHeader class="text-center">
+        <CardTitle class="text-xl">
+          Menus Disponibles
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div class="grid gap-6">
+          <div class="flex flex-col gap-4">
+            <Button variant="outline" class="w-full">
+              Login with Apple
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+    <div
+      class="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
+      Un pequeño menu desarrollado por <a href="https://trinum.xyz" target="_blank" ref="noreferer">Jose Duarte</a>.
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+import { ref, onMounted, watch } from 'vue'
+import CardMenu from '@/components/CardMenu.vue'
+import PocketBase from 'pocketbase'
 
+const menu = ref(null)
+const img_url = ref(null)
+const link = ref('#')
+const ID = '469t9tu2733126p'
+const pb = new PocketBase('https://api-chefsito.trinum.xyz');
+
+onMounted(async () => {
+  menu.value = await pb.collection('restaurants').getOne(ID)
+})
+
+watch(menu, async (newValue) => {
+  if (newValue) {
+    img_url.value = pb.files.getURL(newValue, newValue.image, { thumb: '100x100' })
+    link.value = menu.value.link || '#'
+  }
+})
+
+
+</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div class="flex w-full max-w-sm flex-col gap-6">
+      <a :href="link" class="flex items-center gap-2 self-center font-bold text-lg" target="_blank"
+        rel="noopener noreferrer">
+        <div class="flex items-center justify-center rounded-md bg-primary text-primary-foreground">
+          <img :src="img_url" alt="Logo" class="size-30 rounded object-cover" v-show="img_url" />
+        </div>
+      </a>
+      <CardMenu />
+    </div>
+  </div>
 </template>
